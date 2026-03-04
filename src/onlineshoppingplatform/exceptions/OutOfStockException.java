c5d16316e30b9b3d1a7b196701873b04dc2cbdae
sandeepragavanr-creator <sandeepragavanr@gmail.com>
2026-03-04 23:18:42 +0530

    added comments

--- src/onlineshoppingplatform/exceptions/OutOfStockException.java
+++ src/onlineshoppingplatform/exceptions/OutOfStockException.java
@@ -1,7 +1,7 @@
 package onlineshoppingplatform.exceptions;
 
 public class OutOfStockException  extends RuntimeException{
-
+   // custom Exception
     public OutOfStockException(String message){
         super(message);
     }
